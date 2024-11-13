68c4ef850d70a001b87b1aaff819621a230a27ca
jcanell4 <jcanell4@xtec.cat>
2024-11-13 01:10:09 +0100

    Version 1.1-SNAPSHOT finished. Minor changes

--- src/main/java/org/elsquatrecaps/utilities/tools/configuration/AbstractConfiguration.java
+++ src/main/java/org/elsquatrecaps/utilities/tools/configuration/AbstractConfiguration.java
@@ -11,8 +11,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import org.elsquatrecaps.utilities.tools.Callback;
 
